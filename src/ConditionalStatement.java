3b039089f5c9a64abaed4159c396b593d7770756
jcode <jcodephilosophy@gmail.com>
2021-11-16 18:06:01 +0300

    first commit

---
+++ src/ConditionalStatement.java
@@ -0,0 +1,32 @@
+public class ConditionalStatement{
+    public static void main(String[] args){
+        int age = 21;
+        double heigt = 1.6;
+        char firstLettersName = 'M';
+        boolean male = true;
+
+        if (age > 20) {
+            System.out.println("age more then 20");
+        }
+
+        if (male == true) {
+            System.out.println("male");
+        } else if (male == false) {
+            System.out.println("Not male");
+        }
+
+        if (heigt < 1.80) {
+            System.out.println("height more 1.80");
+        } else {
+            System.out.println("height less 1.80");
+        }
+
+        if (firstLettersName == 'M') {
+            System.out.println("first names letter start with M");
+        } else if (firstLettersName == 'I') {
+            System.out.println("first names letter start with I");
+        } else {
+            System.out.println("note mutch");
+        }
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+        public static void main(String[] args){
+                byte core = 8;
+                short virtualCore = 16;
+                int mhz = 3593;
+                long memory = 32L;
+                float allMemory = 32.0F;
+                double availableMemory = 20.4;
+                char os = 'M';
+                boolean isMyPC = true;
+                System.out.println(core);
+                System.out.println(virtualCore);
+                System.out.println(mhz);
+                System.out.println(memory);
+                System.out.println(allMemory);
+                System.out.println(availableMemory);
+                System.out.println(os);
+                System.out.println(isMyPC);
+        }
+}
